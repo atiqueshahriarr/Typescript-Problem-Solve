69361f7c683455097eec91ebbdafee987d39afd5
atique-shahriar <satique06@gmail.com>
2025-05-07 22:54:19 +0600

    Solve Problem-7

--- TypescriptTask.ts
+++ TypescriptTask.ts
@@ -116,3 +116,24 @@ const products: Product[] = [
 
 console.log(getMostExpensiveProduct(products));
 // Output: { name: "Bag", price: 50 }
+
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+}
+
+function getDayType(day: Day): string {
+    if (day === 6) {
+        return "Weekend";
+    } else {
+        return "Weekday";
+    }
+}
+
+console.log(getDayType(Day.Monday));
+console.log(getDayType(Day.Sunday));
