2671085bf2fac1fc67199296672393248ad57b55
atique-shahriar <satique06@gmail.com>
2025-05-07 21:27:37 +0600

    Solve Problem-2

--- TypescriptTask.ts
+++ TypescriptTask.ts
@@ -8,7 +8,26 @@ function formatString(input: string, toUpper?: boolean): string {
     }
 }
 
-let value1 = formatString("Hello"); // Output: "HELLO"
-let value2 = formatString("Hello", true); // Output: "HELLO"
-let value3 = formatString("Hello", false); // Output: "hello"
+const value1 = formatString("Hello"); // Output: "HELLO"
+const value2 = formatString("Hello", true); // Output: "HELLO"
+const value3 = formatString("Hello", false); // Output: "hello"
 console.log(`${value1}\n${value2}\n${value3}`);
+
+function filterByRating(items: {title: string; rating: number}[]): {title: string; rating: number}[] {
+    let ratingBooks = [];
+    for (const item of items) {
+        if (item.rating >= 4) {
+            ratingBooks.push(item);
+        }
+    }
+    return ratingBooks;
+}
+
+const books: {title: string; rating: number}[] = [
+    {title: "Book A", rating: 4.5},
+    {title: "Book B", rating: 3.2},
+    {title: "Book C", rating: 5.0},
+];
+
+const rateBooks: {title: string; rating: number}[] = filterByRating(books);
+console.log(rateBooks);
