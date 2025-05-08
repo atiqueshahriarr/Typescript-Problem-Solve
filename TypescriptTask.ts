e1c227d7c6eb9065bc41170c159d82008adb1185
atique-shahriar <satique06@gmail.com>
2025-05-08 11:21:33 +0600

    Remove examples and function call

--- TypescriptTask.ts
+++ TypescriptTask.ts
@@ -7,9 +7,6 @@ function formatString(input: string, toUpper?: boolean): string {
         return convertUpper;
     }
 }
-formatString("Hello");
-formatString("Hello", true);
-formatString("Hello", false);
 
 function filterByRating(items: {title: string; rating: number}[]): {title: string; rating: number}[] {
     let ratingBooks = [];
@@ -20,12 +17,6 @@ function filterByRating(items: {title: string; rating: number}[]): {title: strin
     }
     return ratingBooks;
 }
-const books = [
-    {title: "Book A", rating: 4.5},
-    {title: "Book B", rating: 3.2},
-    {title: "Book C", rating: 5.0},
-];
-filterByRating(books);
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
     const concateArray: T[] = [];
@@ -36,8 +27,6 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
     }
     return concateArray;
 }
-concatenateArrays(["a", "b"], ["c"]);
-concatenateArrays([1, 2], [3, 4], [5]);
 
 class Vehicle {
     private make: string;
@@ -60,9 +49,6 @@ class Car extends Vehicle {
         return `Model: ${this.model}`;
     }
 }
-const myCar = new Car("Toyota", 2020, "Corolla");
-myCar.getInfo();
-myCar.getModel();
 
 function processValue(value: string | number): number {
     if (typeof value == "string") {
@@ -73,14 +59,11 @@ function processValue(value: string | number): number {
         return multipliedValue;
     }
 }
-processValue("hello");
-processValue(10);
 
 interface Product {
     name: string;
     price: number;
 }
-
 function getMostExpensiveProduct(products: Product[]): Product | null {
     if (products.length === 0) {
         return null;
@@ -96,12 +79,6 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
         return products[highestPriceIndx];
     }
 }
-const products = [
-    {name: "Pen", price: 10},
-    {name: "Notebook", price: 25},
-    {name: "Bag", price: 50},
-];
-getMostExpensiveProduct(products);
 
 enum Day {
     Monday,
@@ -119,8 +96,6 @@ function getDayType(day: Day): string {
         return "Weekday";
     }
 }
-getDayType(Day.Monday);
-getDayType(Day.Sunday);
 
 async function squareAsync(n: number): Promise<number> {
     return new Promise((resolve, reject) => {
@@ -133,5 +108,3 @@ async function squareAsync(n: number): Promise<number> {
         }
     });
 }
-squareAsync(4).then(console.log);
-squareAsync(-3).catch(console.error);
